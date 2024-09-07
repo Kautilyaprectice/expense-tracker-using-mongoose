ac415ab5541d1901ef5c9c02994e67fb4bc1bf0b
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-09-07 17:38:07 +0530

    Update login.js

--- public/login/login.js
+++ public/login/login.js
@@ -11,7 +11,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event){
 
     if(email && password){
 
-        axios.post('http://localhost:3000/user/login', loginDetails)
+        axios.post('https://expense-tracker-using-mongoose-xt7ie0020.vercel.app', loginDetails)
             .then((res) => {
                 if(res.status === 200){
                     alert("Login Successful");
