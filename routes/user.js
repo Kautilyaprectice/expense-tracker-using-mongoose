8c495be0f8de997c1366e32e27f58eff5da4b6a4
Kautilya Tiwari <160465716+Kautilyaprectice@users.noreply.github.com>
2024-09-07 17:40:04 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userAuthentication = require('../middlewares/authenticate');
 
 router.post('/user/signup', userController.createUser);
-router.post('/user/login', userController.loginUser);
+router.post('/', userController.loginUser);
 
 
 module.exports = router;
